f0d91f419cac2abb69192fed2cdce804621ccf69
KAPIL BASNET <163415277+KapilBasnet1999@users.noreply.github.com>
2025-02-16 19:05:18 -0600

    Add files via upload

---
+++ HomeWork4.cs
@@ -0,0 +1,125 @@
+﻿// Written by Kapil
+// Feb 16 2025
+
+using System;
+using System.Linq;
+
+namespace HomeWork4
+{
+    public class Homework
+    {
+        // Private variables
+        private string sentence= "n/a";
+        private string inputString = "n/a";
+        private int nVowels = -1;
+        private string inputStr = "n/a";
+
+        // Properties (Getters and Setters)
+        public string Sentence
+        {
+            get { return this.sentence; }
+            set { this.sentence = value; }
+        }
+
+        public string InputString
+        {
+            get { return this.inputString; }
+            set { this.inputString = value; }
+        }
+
+        public int NVowels
+        {
+            get { return this.nVowels; }
+            set { this.nVowels = value; }
+        }
+
+        public string InputStr
+        {
+            get { return this.inputStr; }
+            set { this.inputStr = value; }
+        }
+
+        // Constructor
+        public Homework(string sentence, string inputString, int nVowels, string inputStr)
+        {
+            this.Sentence = sentence;
+            this.InputString = inputString;
+            this.NVowels = nVowels;
+            this.InputStr = inputStr;
+        }
+
+        // 1) Count words in a sentence
+        public int CountWords()
+        {
+            if (string.IsNullOrEmpty(Sentence))
+            {
+                return 0; // Return 0 if sentence is empty or null
+            }
+            return Sentence.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Length;
+        }
+
+        // 2) Swap first and last characters of the input string
+        public string FlipEndChars()
+        {
+            if (string.IsNullOrEmpty(InputString) || InputString.Length < 2)
+            {
+                return "Incompatible."; // If string length is less than 2
+            }
+
+            if (InputString[0] == InputString[InputString.Length - 1])
+            {
+                return "Two's a pair."; // If first and last characters are the same
+            }
+
+            char[] chars = InputString.ToCharArray();
+            char temp = chars[0];
+            chars[0] = chars[InputString.Length - 1];
+            chars[InputString.Length - 1] = temp;
+            return new string(chars);
+        }
+
+        // 3) Check if characters are in order
+        public bool IsInOrder()
+        {
+            string sortedString = string.Concat(InputStr.OrderBy(c => c));
+            return InputStr == sortedString;
+        }
+
+        // 4) Return the first N vowels of the input string
+        public string FirstNVowels()
+        {
+            string vowels = "aeiou";
+            string result = new string(InputStr.Where(c => vowels.Contains(c)).ToArray());
+            return result.Length >= NVowels ? result.Substring(0, NVowels) : "invalid";
+        }
+
+        // 5) Change every letter in the string to the next letter
+        public string MoveLetters()
+        {
+            char[] result = new char[InputStr.Length];
+            for (int i = 0; i < InputStr.Length; i++)
+            {
+                if (char.IsLower(InputStr[i]))
+                {
+                    result[i] = (char)(InputStr[i] + 1);
+                }
+                else
+                {
+                    result[i] = InputStr[i];
+                }
+            }
+            return new string(result);
+        }
+
+        // Overriding ToString method 
+        public override string ToString()
+        {
+            string message = "";
+            message += "Input String: " + this.InputString + "\n";
+            message += "Sentence: " + this.Sentence + "\n";
+            message += "N Vowels: " + this.NVowels + "\n";
+            message += "Input String for Transformation: " + this.InputStr + "\n";
+            return message;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,101 @@
+﻿// Written by Kapil
+// Feb 16 2025
+
+using System;
+using System.Collections.Generic;
+
+namespace HomeWork4
+{
+    public class Program
+    {
+        public static void Main(string[] arg)
+        {
+            // Displaying available tasks to the user
+            Console.WriteLine("Select an Option:");
+            Console.WriteLine("1. Word Count");
+            Console.WriteLine("2. Flip End Characters");
+            Console.WriteLine("3. Check if Characters Are in Order");
+            Console.WriteLine("4. First N Vowels");
+            Console.WriteLine("5. Change Every Letter to the Next Letter");
+
+            // Read user input for task choice
+            Console.Write("\nSelect an Option between 1-5:");
+            int taskChoice = int.Parse(Console.ReadLine()) - 1; // Subtract 1 to match index
+
+            // Make sure task number is valid
+            if (taskChoice >= 0 && taskChoice < 5)
+            {
+                // Call the method based on the task choice
+                switch (taskChoice)
+                {
+                    case 0:
+                        CountWordsTask();
+                        break;
+                    case 1:
+                        FlipEndCharsTask();
+                        break;
+                    case 2:
+                        IsInOrderTask();
+                        break;
+                    case 3:
+                        FirstNVowelsTask();
+                        break;
+                    case 4:
+                        MoveLettersTask();
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid task number.");
+            }
+        }
+
+        // 1. Word Count Task
+        private static void CountWordsTask()
+        {
+            Console.Write("\nEnter a sentence to count the words:");
+            string inputSentence = Console.ReadLine();
+            Homework homework = new Homework(inputSentence, "", 0, "");
+            Console.WriteLine($"Word count: {homework.CountWords()}");
+        }
+
+        // 2. Flip End Characters Task
+        private static void FlipEndCharsTask()
+        {
+            Console.Write("\nEnter a string to flip the first and last characters:");
+            string inputString = Console.ReadLine();
+            Homework homework = new Homework("", inputString, 0, "");
+            Console.WriteLine($"Result: {homework.FlipEndChars()}");
+        }
+
+        // 3. Check if Characters Are in Order Task
+        private static void IsInOrderTask()
+        {
+            Console.Write("\nEnter a string to check if the characters are in order:");
+            string inputStr = Console.ReadLine();
+            Homework homework = new Homework("", "", 0, inputStr);
+            Console.WriteLine($"Is in order: {homework.IsInOrder()}");
+        }
+
+        // 4. First N Vowels Task
+        private static void FirstNVowelsTask()
+        {
+            Console.Write("\nEnter a string to extract vowels and specify how many vowels to extract:");
+            string inputStr = Console.ReadLine();
+            Console.Write("Enter the number of vowels to extract:");
+            int nVowels = int.Parse(Console.ReadLine());
+            Homework homework = new Homework("", "", nVowels, inputStr);
+            Console.WriteLine($"First {nVowels} vowels: {homework.FirstNVowels()}");
+        }
+
+        // 5. Change Every Letter to the Next Letter Task
+        private static void MoveLettersTask()
+        {
+            Console.Write("\nEnter a string to change every letter to the next letter:");
+            string inputStr = Console.ReadLine();
+            Homework homework = new Homework("", "", 0, inputStr);
+            Console.WriteLine($"Changed string: {homework.MoveLetters()}");
+        }
+    }
+}
